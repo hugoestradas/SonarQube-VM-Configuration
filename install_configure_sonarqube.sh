63e445240c89da17585d7b031982f79aff66c393
Hugo Estrada <61108175+hugoestradas@users.noreply.github.com>
2020-03-20 15:30:31 -0600

    Update install_configure_sonarqube.sh

--- install_configure_sonarqube.sh
+++ install_configure_sonarqube.sh
@@ -8,15 +8,12 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm -f *microsoft*.deb
 
 sudo apt-get install apt-transport-https
-sudo apt-get update
 sudo apt-get install dotnet-sdk-3.1
 
 sudo apt-get install apt-transport-https
-sudo apt-get update
 sudo apt-get install aspnetcore-runtime-3.1
 
 sudo apt-get install apt-transport-https
-sudo apt-get update
 sudo apt-get install dotnet-runtime-3.1
 
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip

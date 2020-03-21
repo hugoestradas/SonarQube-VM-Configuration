a54b81365678cd9f1bfb85202481c82cf2e69013
Hugo Estrada <61108175+hugoestradas@users.noreply.github.com>
2020-03-20 22:06:09 -0600

    Update install_configure_sonarqube.sh

--- install_configure_sonarqube.sh
+++ install_configure_sonarqube.sh
@@ -8,12 +8,16 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm -f *microsoft*.deb
 
 sudo apt-get install apt-transport-https
+sudo apt-get update
 sudo apt-get install dotnet-sdk-3.1
+sudo apt-get install dotnet-sdk-2.1
 
 sudo apt-get install apt-transport-https
+sudo apt-get update
 sudo apt-get install aspnetcore-runtime-3.1
 
 sudo apt-get install apt-transport-https
+sudo apt-get update
 sudo apt-get install dotnet-runtime-3.1
 
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
@@ -21,3 +25,19 @@ sudo unzip -d /opt/ sonar-scanner-cli*.zip
 rm -f *zip
 wget https://github.com/SonarSource/sonar-scanning-examples/archive/master.zip
 unzip master.zip
+rm -f master.zip
+
+echo "© 2020 Conduent, Inc. All rights reserved."
+echo "In the file /opt/sonar-scanner*-linux/conf/sonar-scanner.properties
+      add the following lines (copy and paste them) as described below:
+        sonar.host.url=https://< localhost | ip of the VM >:9000
+        sonar.sourceEncoding=UTF-9"
+read -p "Press ENTER key to continue." enter
+sudo vim /opt/sonar-scanner-4.2.0.1873-linux/conf/sonar-scanner.properties
+echo "In the file /etc/profile.d/sonar-scanner.sh add the following lines (copy and pasted them) as described below:"
+echo "#/bin/bash"
+echo "export PATH=$PATH:/opt/sonar-scanner-4.2.0.1873-linux/bin"
+read -p "Press ENTER key to continue." secondenter
+sudo vim /etc/profile.d/sonar-scanner.sh
+
+dotnet tool install --global dotnet-sonarscanner --version 4.7.1

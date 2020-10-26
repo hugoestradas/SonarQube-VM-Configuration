bad7169dc6f8f8fe8711848acd51ef4874ed0cd2
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2020-10-26 10:52:10 -0600

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,7 +1,13 @@
 az vm image list --location westus --publisher Bitnami --all --output table
+
 az group create --name "poccodeanalysisrg" --location "eastus"
+
 az vm create --resource-group "poccodeanalysisrg" -n "vm" --image "Bitnami:sonarqube:6-4:8.2.2003100519" --size "Standard_B4ms" --admin-username "vmuser" --admin-password "Password@123" -l "eastus"
+
 az vm open-port --port 22 --resource-group "poccodeanalysisrg" -n "vm" --priority 100
+
 az vm open-port --port 80 --resource-group "poccodeanalysisrg" -n "vm" --priority 101
+
 az vm open-port --port 900 --resource-group "poccodeanalysisrg" -n "vm" --priority 102
+
 az vm show -d -g "poccodeanalysisrg" -n "vm" --query "publicIps" -o tsv
